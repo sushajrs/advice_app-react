562aa8b3e49d9f8d8f7163c405c763dd111a3aac
sushajrs <sushajxui@gmail.com>
2020-04-28 14:55:02 +0530

    'initial-commit'

---
+++ .history/src/App_20200330144003.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css'
+
+class App extends React.Component {
+  state = { advice: '' };
+  render() {
+    return(
+      <h1>APP</h1>
+    );
+  }
+} 
+export default App;
